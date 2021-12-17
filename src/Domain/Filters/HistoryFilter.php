b212860af9852495e9b93fef09f773bce487f44f
ZnDeveloper <zndev@ya.ru>
2021-12-18 01:10:20 +0600

    upd

--- src/Domain/Filters/HistoryFilter.php
+++ src/Domain/Filters/HistoryFilter.php
@@ -2,14 +2,30 @@
 
 namespace ZnUser\Notify\Domain\Filters;
 
-use ZnSandbox\Sandbox\Status\Domain\Filters\BaseStatusFilter;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
+use ZnCore\Domain\Constraints\Enum;
+use ZnCore\Domain\Interfaces\Entity\ValidateEntityByMetadataInterface;
+use ZnSandbox\Sandbox\Status\Domain\Enums\StatusEnum;
 
-class HistoryFilter extends BaseStatusFilter
+class HistoryFilter implements ValidateEntityByMetadataInterface
 {
+
+    protected $statusId = StatusEnum::ENABLED;
 
     public static function loadValidatorMetadata(ClassMetadata $metadata)
     {
-        self::loadStatusValidatorMetadata($metadata);
+        $metadata->addPropertyConstraint('statusId', new Enum([
+            'class' => StatusEnum::class,
+        ]));
+    }
+
+    public function setStatusId(int $value): void
+    {
+        $this->statusId = $value;
+    }
+
+    public function getStatusId()
+    {
+        return $this->statusId;
     }
 }
