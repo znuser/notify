67e0df58e40e1c85f5fc14539d0d62825d722eca
ZnDeveloper <zndev@ya.ru>
2022-01-17 15:49:52 +0600

    upd

---
+++ src/Domain/Migrations/m_2022_01_13_072456_add_unique_transport_table.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Migrations;
+
+use Illuminate\Database\Schema\Blueprint;
+use ZnLib\Migration\Domain\Base\BaseColumnMigration;
+
+class m_2022_01_13_072456_add_unique_transport_table extends BaseColumnMigration
+{
+    protected $tableName = 'notify_transport';
+
+    public function tableSchema()
+    {
+        return function (Blueprint $table) {
+            $table->unique('name');
+        };
+    }
+}
