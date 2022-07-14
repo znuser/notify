7df89a88e2f6a58ef5edba8748ec95077d39e975
Vitaliy <theyamshikov@yandex.ru>
2022-07-14 12:21:14 +0600

    upd

--- src/Domain/Filters/HistoryFilter.php
+++ src/Domain/Filters/HistoryFilter.php
@@ -5,17 +5,17 @@ namespace ZnUser\Notify\Domain\Filters;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 use ZnCore\Enum\Constraints\Enum;
 use ZnCore\Validation\Interfaces\ValidationByMetadataInterface;
-use ZnSandbox\Sandbox\Status\Domain\Enums\StatusEnum;
+use ZnLib\Components\Status\Enums\StatusSimpleEnum;
 
 class HistoryFilter implements ValidationByMetadataInterface
 {
 
-    protected $statusId = StatusEnum::ENABLED;
+    protected $statusId = StatusSimpleEnum::ENABLED;
 
     public static function loadValidatorMetadata(ClassMetadata $metadata)
     {
         $metadata->addPropertyConstraint('statusId', new Enum([
-            'class' => StatusEnum::class,
+            'class' => StatusSimpleEnum::class,
         ]));
     }
 
